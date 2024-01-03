25b6e2b7b5158d875e22f62a1b35af6f2af7f4d2
akshaycs435 <anaghasasidharan38@gmail.com>
2024-01-03 17:34:52 +0530

    add new user resgistration in admin

--- newapp/models.py
+++ newapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    first_name=models.CharField(max_length=20)
+    last_name=models.CharField(max_length=20)
+    email_address=models.EmailField(max_length=50)
+    phone_no=models.CharField(max_length=12)
+    place=models.CharField(max_length=50)
--- newapp/views.py
+++ newapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Users
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    mem=Users.objects.all()
+    return render(request,'index.html',{'mem':mem})
